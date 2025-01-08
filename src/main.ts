715bb0ee763ea1426196eeda68913bbf08e7372e
tw0ten <twoten.eq@gmail.com>
2025-01-08 18:29:51 +0200

    TA

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,34 @@
-import l from "l";
+import { Logger } from "l";
 import secrets from "../io/o/secre.ts";
 import { Agent, CredentialSession } from "@atproto/api";
 
-l.i(">");
-l.n("main");
+const l = new Logger("main");
 
+l.i(">");
 {
 	const credentials = new CredentialSession(new URL(secrets.bluesky.service));
-	l.i("login..");
-	await credentials.login({
-		identifier: secrets.bluesky.identifier,
-		password: secrets.bluesky.password,
-	});
-	l.i(credentials.did, credentials.serviceUrl.host);
+
+	l.n("auth");
+	{
+		try {
+			l.i("resume");
+			await credentials.resumeSession(
+				JSON.parse(Deno.readTextFileSync("o/session")),
+			);
+		} catch {
+			l.i("login");
+			await credentials.login(secrets.bluesky);
+		}
+		l.i(credentials.did, credentials.serviceUrl.host);
+		Deno.writeTextFile("o/session", JSON.stringify(credentials.session));
+	}
+	l.u();
 
 	await import("@/bot.ts")
 		.then((m) => m.default(new Agent(credentials)))
 		.catch(l.e);
 
-	l.i("logout..");
+	l.i("logout");
 	await credentials.logout();
-	l.i(".");
 }
-
-l.u();
 l.i("<");
